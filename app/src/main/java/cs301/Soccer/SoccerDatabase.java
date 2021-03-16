9c60a05365147a8b992ad62132e436dd896b163c
Gareth <riceg23@up.edu>
2021-03-16 14:44:26 -0700

    Add all required functionality

--- app/src/main/java/cs301/Soccer/SoccerDatabase.java
+++ app/src/main/java/cs301/Soccer/SoccerDatabase.java
@@ -3,6 +3,10 @@ package cs301.Soccer;
 import android.util.Log;
 import cs301.Soccer.soccerPlayer.SoccerPlayer;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 /**
@@ -15,7 +19,7 @@ import java.util.*;
 public class SoccerDatabase implements SoccerDB {
 
     // dummied up variable; you will need to change this
-    private Hashtable database;
+    private Hashtable<String, SoccerPlayer> database = new Hashtable();
 
     /**
      * add a player
@@ -25,7 +29,24 @@ public class SoccerDatabase implements SoccerDB {
     @Override
     public boolean addPlayer(String firstName, String lastName,
                              int uniformNumber, String teamName) {
-        return false;
+
+        boolean isSuccessful = false;
+
+        String fullName = firstName + " ## " + lastName;
+
+        if(database.containsKey(fullName) || firstName.equals("") || lastName.equals("") ||
+                uniformNumber == 0 || teamName.equals("")){
+            isSuccessful = false;
+        }else{
+            //instantiate new soccer player
+            SoccerPlayer toAdd = new SoccerPlayer(firstName, lastName, uniformNumber, teamName);
+
+            //add the soccer player to the database with fullName key
+            database.put(fullName, toAdd);
+            isSuccessful = true;
+        }
+
+        return isSuccessful;
     }
 
     /**
@@ -35,7 +56,18 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean removePlayer(String firstName, String lastName) {
-        return false;
+        boolean isSuccessful = false;
+
+        String fullNameKey = firstName + " ## " + lastName;
+
+        if(database.containsKey(fullNameKey)){
+            database.remove(fullNameKey);
+            isSuccessful = true;
+        }else{
+            isSuccessful = false;
+        }
+
+        return isSuccessful;
     }
 
     /**
@@ -45,7 +77,17 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public SoccerPlayer getPlayer(String firstName, String lastName) {
-        return null;
+        SoccerPlayer retrievePlayer;
+
+        String fullNameKey = firstName + " ## " + lastName;
+
+        if(database.containsKey(fullNameKey)){
+            retrievePlayer = database.get(fullNameKey);
+        }else{
+            retrievePlayer = null;
+        }
+
+        return retrievePlayer;
     }
 
     /**
@@ -55,7 +97,19 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpGoals(String firstName, String lastName) {
-        return false;
+        boolean isSuccessful = false;
+
+        String fullNameKey = firstName + " ## " + lastName;
+
+        if(database.containsKey(fullNameKey)){
+            SoccerPlayer selected = database.get(fullNameKey);
+            selected.bumpGoals();
+            isSuccessful = true;
+        }else{
+            isSuccessful = false;
+        }
+
+        return isSuccessful;
     }
 
     /**
@@ -65,7 +119,19 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpYellowCards(String firstName, String lastName) {
-        return false;
+        boolean isSuccessful = false;
+
+        String fullNameKey = firstName + " ## " + lastName;
+
+        if(database.containsKey(fullNameKey)){
+            SoccerPlayer selected = database.get(fullNameKey);
+            selected.bumpYellowCards();
+            isSuccessful = true;
+        }else{
+            isSuccessful = false;
+        }
+
+        return isSuccessful;
     }
 
     /**
@@ -75,7 +141,19 @@ public class SoccerDatabase implements SoccerDB {
      */
     @Override
     public boolean bumpRedCards(String firstName, String lastName) {
-        return false;
+        boolean isSuccessful = false;
+
+        String fullNameKey = firstName + " ## " + lastName;
+
+        if(database.containsKey(fullNameKey)){
+            SoccerPlayer selected = database.get(fullNameKey);
+            selected.bumpRedCards();
+            isSuccessful = true;
+        }else{
+            isSuccessful = false;
+        }
+
+        return isSuccessful;
     }
 
     /**
@@ -86,7 +164,20 @@ public class SoccerDatabase implements SoccerDB {
     @Override
     // report number of players on a given team (or all players, if null)
     public int numPlayers(String teamName) {
-        return -1;
+        int toReturn = -1;
+
+        if(teamName == null){
+            toReturn = database.size();
+        }else{
+            toReturn = 0;
+            for(SoccerPlayer curr : database.values()){
+                if(curr.getTeamName().equals(teamName)){
+                    toReturn++;
+                }
+            }
+        }
+
+        return toReturn;
     }
 
     /**
@@ -97,7 +188,28 @@ public class SoccerDatabase implements SoccerDB {
     // get the nTH player
     @Override
     public SoccerPlayer playerIndex(int idx, String teamName) {
-        return null;
+        SoccerPlayer toReturn = null;
+
+        int increment = -1;
+
+        if(idx > numPlayers(teamName)){
+            toReturn = null;
+        }else{
+            for(SoccerPlayer curr : database.values()){
+                //if the team name is empty, return
+                if(teamName == null){
+                    increment++;
+                    if(increment == idx) return curr;
+                }else{
+                    if(curr.getTeamName().equals(teamName)){
+                        increment++;
+                        if(increment == idx) return curr;
+                    }
+                }
+            }//iterate over soccer players
+        }
+
+        return toReturn;
     }
 
     /**
@@ -108,6 +220,55 @@ public class SoccerDatabase implements SoccerDB {
     // read data from file
     @Override
     public boolean readData(File file) {
+        Scanner fileIn;
+
+        if(file == null){
+            return false;
+        }
+
+        try{
+            fileIn = new Scanner(file);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return file.exists();
+        }
+
+        while(fileIn.hasNext()){
+            String firstN = fileIn.nextLine();
+            String lastN = fileIn.nextLine();
+            String teamN = fileIn.nextLine();
+            int uniNum = Integer.parseInt(fileIn.nextLine());
+
+            SoccerPlayer player = new SoccerPlayer(firstN, lastN, uniNum, teamN);
+
+            int goals = Integer.parseInt(fileIn.nextLine());
+            int yellows = Integer.parseInt(fileIn.nextLine());
+            int reds = Integer.parseInt(fileIn.nextLine());
+
+            for(int i = 0; i < goals; i++){
+                player.bumpGoals();
+            }
+
+            for(int i = 0; i < yellows; i++){
+                player.bumpYellowCards();
+            }
+
+            for(int i = 0; i < reds; i++){
+                player.bumpRedCards();
+            }
+
+            String key = firstN + " ## " + lastN;
+
+            //clear old entry
+            if(database.containsKey(key)){
+                database.remove(key);
+            }
+
+            database.put(key, player);
+        }
+
+        fileIn.close();
+
         return file.exists();
     }
 
@@ -119,7 +280,27 @@ public class SoccerDatabase implements SoccerDB {
     // write data to file
     @Override
     public boolean writeData(File file) {
-        return false;
+        PrintWriter pw = null;
+        try{
+            pw = new PrintWriter(file);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return false;
+        }
+
+        for(SoccerPlayer play : database.values()){
+            pw.println(logString(play.getFirstName()));
+            pw.println(logString(play.getLastName()));
+            pw.println(logString(play.getTeamName()));
+            pw.println(logString(Integer.toString(play.getUniform())));
+            pw.println(logString(Integer.toString(play.getGoals())));
+            pw.println(logString(Integer.toString(play.getYellowCards())));
+            pw.println(logString(Integer.toString(play.getRedCards())));
+        }
+
+        pw.close();
+
+        return true;
     }
 
     /**
